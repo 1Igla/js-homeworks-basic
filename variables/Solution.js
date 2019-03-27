0b0900a81bf9ffd6d942661ad9801df82c7a3640
1Igla <elena25-2025@ya.ru>
2019-03-27 17:59:23 +0300

    Initial commit

---
+++ variables/Solution.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var nameProduct;
+var priceProduct;
+
+nameProduct = '«Телепорт бытовой VZHIH-101»';
+priceProduct = '10000';
+console.log();
+
+console.log(`В наличии имеется: ${nameProduct}`);
+console.log(`Стоимость товара, ${priceProduct}, Q`);
+console.log();
+
+var quantityProduct = 2;
+var discountPercentage = 0.1;
+
+
+var paymentSumm = priceProduct * quantityProduct - priceProduct * quantityProduct * discountPercentage;
+console.log(`Цена покупки составит ${paymentSumm} Q`);
+console.log();
+
+var totalBudget = 52334224;
+var purchasePrice = 6500;
+
+var restAmount = totalBudget % purchasePrice;
+var numPurсhaseProduct = (totalBudget - restAmount) / purchasePrice;
+
+console.log(
+	`Мы можем закупить ${numPurсhaseProduct} единиц товара, после закупки на счету останется ${restAmount} Q`
+);
